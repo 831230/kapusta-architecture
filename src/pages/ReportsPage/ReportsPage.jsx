1089683d00999ad31917fb30978443e0708e10cc
Tomasz Bielecki <szkoleniegoit@gmail.com>
2024-05-14 21:57:30 +0200

    chart

--- src/pages/ReportsPage/ReportsPage.jsx
+++ src/pages/ReportsPage/ReportsPage.jsx
@@ -14,7 +14,7 @@ import CurrentPeriod from "../../components/CurrentPeriod/CurrentPeriod";
 import BalanceLabel from "../../components/BalanceLabel/BalanceLabel";
 import ReportsCategoriesNav from "../../components/ReportsCategoriesNav/ReportsCategoriesNav";
 import CategoryList from "../../components/CategoryList/CategoryList";
-import Chart from "../../components/Chart/Chart";
+import BarChart from "../../components/Chart/BarChart";
 
 const ReportsPage = () => {
   const [currentPeriod, setCurrentPeriod] = useState();
@@ -25,24 +25,75 @@ const ReportsPage = () => {
   useEffect(() => {
     dispatch(getTransactionsData(currentPeriod));
   }, [dispatch, currentPeriod]);
-  return ( 
+  return (
     <>
       <div className="reports-header">
         <Link to="/transactions/expenses">Main page</Link>
-        <Balance/>
-        <CurrentPeriod currentPeriod={currentPeriod}/>
+        <Balance />
+        <CurrentPeriod currentPeriod={currentPeriod} />
       </div>
       <div className="reports-content">
         <BalanceLabel />
-        <ReportsCategoriesNav/>
-      <div className="category-list">
-        <CategoryList/>
-      </div>
-        <Chart/>
+        <ReportsCategoriesNav />
+        <div className="category-list">
+          <CategoryList />
+        </div>
+        <BarChart />
       </div>
     </>
-    
-   );
+
+  );
 }
- 
+
 export default ReportsPage;
+
+
+
+// import { useState, useEffect } from "react";
+// import { Link } from "react-router-dom";
+// import { useDispatch } from "react-redux";
+
+// // REDUX HOOKS:
+// import useReports from "../../hooks/useReports";
+
+// // REDUX ACTIONS:
+// import { getTransactionsData } from "../../redux/reports/operators";
+
+// // COMPONENTS:
+// import Balance from "../../components/Balance/Balance";
+// import CurrentPeriod from "../../components/CurrentPeriod/CurrentPeriod";
+// import BalanceLabel from "../../components/BalanceLabel/BalanceLabel";
+// import ReportsCategoriesNav from "../../components/ReportsCategoriesNav/ReportsCategoriesNav";
+// import CategoryList from "../../components/CategoryList/CategoryList";
+// import Chart from "../../components/Chart/Chart";
+
+// const ReportsPage = () => {
+//   const [currentPeriod, setCurrentPeriod] = useState();
+//   const { incomesReport, expensesReport } = useReports();
+//   // console.log({ incomesReport, expensesReport });
+//   const dispatch = useDispatch();
+
+//   useEffect(() => {
+//     dispatch(getTransactionsData(currentPeriod));
+//   }, [dispatch, currentPeriod]);
+//   return ( 
+//     <>
+//       <div className="reports-header">
+//         <Link to="/transactions/expenses">Main page</Link>
+//         <Balance/>
+//         <CurrentPeriod currentPeriod={currentPeriod}/>
+//       </div>
+//       <div className="reports-content">
+//         <BalanceLabel />
+//         <ReportsCategoriesNav/>
+//       <div className="category-list">
+//         <CategoryList/>
+//       </div>
+//         <Chart/>
+//       </div>
+//     </>
+    
+//    );
+// }
+ 
+// export default ReportsPage;
