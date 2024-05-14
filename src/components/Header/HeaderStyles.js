5e7b6f2ade44a11095438f24dc263312fa1c2466
Tomasz Bielecki <szkoleniegoit@gmail.com>
2024-05-14 20:02:58 +0200

    header component

---
+++ src/components/Header/HeaderStyles.js
@@ -0,0 +1,61 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+export const HeaderContainer = styled.header`
+  display: flex;
+  width: 100%;
+  justify-content: center;
+
+  background-color: #fff;
+`;
+export const HeaderWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 1280px;
+
+  padding: 12px;
+
+  @media screen and (max-width: 768px) {
+    width: 768px;
+    padding-left: 32px;
+    padding-right: 32px;
+  }
+  @media screen and (max-width: 320px) {
+    width: 320px;
+    padding-left: 20px;
+    padding-right: 20px;
+  }
+`;
+
+export const HeaderLogoLink = styled(Link)`
+  width: 90px;
+  height: 31px;
+`;
+
+export const HeaderUserContainer = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 40px;
+
+  @media screen and (max-width: 320px) {
+    gap: 16px;
+  }
+`;
+
+export const HeaderUserDivider = styled.div`
+  width: 1px;
+  height: 16px;
+
+  margin: 0 19.5px;
+  background-color: #e0e5eb;
+
+  position: absolute;
+  right: 25px;
+
+  @media screen and (max-width: 320px) {
+    display: none;
+  }
+`;
