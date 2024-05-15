f01805b2aafe03d1e8557a72b95081bafa012548
Tomasz Bielecki <szkoleniegoit@gmail.com>
2024-05-15 10:30:54 +0200

    component balance

--- src/components/Balance/Balance.jsx
+++ src/components/Balance/Balance.jsx
@@ -1,24 +1,49 @@
-import { useState } from "react";
+import {
+  BalanceForm,
+  BalanceLabel,
+  BalanceInput,
+  BalanceButton,
+} from "./BalanceStyles";
+import { useRef } from "react";
 import { useDispatch } from "react-redux";
 
 // REDUX ACTIONS:
 import { updateBalance } from "../../redux/user/operations";
 
+// REDUX HOOKS:
+import useAuth from "../../hooks/useAuth";
+
 const Balance = () => {
-  const [balanceValue, setBalanceValue] = useState("");
-
+  const form = useRef();
+  const { userData } = useAuth();
+  let UserBalance = userData.balance;
+  if (UserBalance === null) {
+    UserBalance = 0;
+  }
   const dispatch = useDispatch();
+  let balanceValue;
 
-  const setNewBalance = () => dispatch(updateBalance(balanceValue))
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    balanceValue = evt.target.balance.value;
+    dispatch(updateBalance(balanceValue));
+    form.current.reset();
+  };
 
+  return (
+    <BalanceForm onSubmit={handleSubmit} ref={form}>
+      <BalanceLabel>Balance:</BalanceLabel>
+      <BalanceInput
+        type="number"
+        name="balance"
+        title="Please, enter your balance"
+        value={balanceValue}
+        placeholder={`${UserBalance}.00 USD`}
+        required
+      />
+      <BalanceButton type="submit">CONFIRM</BalanceButton>
+    </BalanceForm>
+  );
+};
 
-  return ( 
-    <div>
-      <span>Balance</span>
-      <input type="number" onChange={(e) => setBalanceValue(e.target.value)} value={balanceValue}/>
-      <button type="button" onClick={setNewBalance}>Confirm</button>
-    </div>
-   );
-}
- 
 export default Balance;
