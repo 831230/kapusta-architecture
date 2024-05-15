8d35853b571a51d966d6725931bf12c541588bf4
KZapal <kamil.zapal@o2.pl>
2024-05-15 10:27:32 +0200

    Complete Summary Component

--- src/components/ExpensesIncomeSummary/ExpensesIncomeSummary.jsx
+++ src/components/ExpensesIncomeSummary/ExpensesIncomeSummary.jsx
@@ -1,22 +1,59 @@
-
 // REDUX HOOKS:
 import useExpenses from "../../hooks/useExpenses";
-
+import useIncomes from "../../hooks/useIncomes";
+import { useLocation } from "react-router-dom";
+import { isBefore, parse } from "date-fns";
+import {
+  SummaryBox,
+  SummaryItem,
+  SummaryList,
+  SummaryTitle,
+} from "./Summary.styles";
 
 const ExpensesIncomeSummary = () => {
-  const {expenses, expensesMonthStats} = useExpenses();
-  // console.log("ExpensesIncomeSummary:",{expenses, expensesMonthStats});
+  const location = useLocation();
 
-  return ( 
-    <div>
-      <h4>Summary</h4>
-      <p>January</p>
-      <p>February</p>
-      <p>March</p>
-      <p>April</p>
-      <p>...</p>
-    </div>
-   );
-}
- 
+  const { expensesMonthStats } = useExpenses();
+  const { incomesMonthStats } = useIncomes();
+
+  const route = location.pathname;
+  let data;
+  let stats;
+
+  if (route === "/transactions/expenses") {
+    stats = expensesMonthStats;
+  }
+  if (route === "/transactions/income") {
+    stats = incomesMonthStats;
+  }
+
+  const currentDate = new Date();
+
+  if (stats) {
+    data = Object.entries(stats)
+      .filter(([month]) => {
+        const monthDate = parse(month, "MMMM", new Date());
+        return isBefore(monthDate, currentDate);
+      })
+      .map(([month, value]) => ({
+        month: month,
+        value: value,
+      }));
+  }
+
+  return (
+    <SummaryBox>
+      <SummaryTitle>Summary</SummaryTitle>
+      <SummaryList>
+        {data.map((item, index) => (
+          <SummaryItem key={index}>
+            <div>{item.month}</div>
+            <div>{item.value}</div>
+          </SummaryItem>
+        ))}
+      </SummaryList>
+    </SummaryBox>
+  );
+};
+
 export default ExpensesIncomeSummary;
