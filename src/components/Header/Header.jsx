a66845e3f48a9556fcc757f5dbeeed0ea4e07e39
Piotr Poterek <peterpoterek0@gmail.com>
2024-05-18 18:11:48 +0200

    header logo moved to the seperate file

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import useAuth from "../../hooks/useAuth";
 import UserData from "../UserData/UserData.jsx";
 import ExitButton from "../ExitButton/ExitButton.jsx";
-import logo from "./logo.svg";
+import logo from "../../assets/header-logo.svg";
 import styles from "./HeaderStyles.module.css";
 
 const Header = () => {
