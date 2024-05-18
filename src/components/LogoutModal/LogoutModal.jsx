bf2891e7616b93a71cc60d0f88811303c3ba0cda
Piotr Poterek <peterpoterek0@gmail.com>
2024-05-18 17:33:07 +0200

    styled components changed into css modules

--- src/components/LogoutModal/LogoutModal.jsx
+++ src/components/LogoutModal/LogoutModal.jsx
@@ -1,9 +1,7 @@
-import { ModalContainer, ModalWrapper, ModalText, ModalButtonContainer, ModalButton, ModalButtonText } from "./LogoutModalStyles";
-
+import React from "react";
 import { useDispatch } from "react-redux";
-
-// REDUX ACTIONS:
 import { logout } from "../../redux/user/operations";
+import styles from "./LogoutModalStyles.module.css";
 
 const LogoutModal = ({ modalText, isOpen, toggleModal }) => {
   const dispatch = useDispatch();
@@ -15,20 +13,19 @@ const LogoutModal = ({ modalText, isOpen, toggleModal }) => {
 
   return (
     isOpen && (
-      <ModalContainer>
-        <ModalWrapper>
-          <ModalText>{modalText}</ModalText>
-
-          <ModalButtonContainer>
-            <ModalButton onClick={handleUserLogout}>
-              <ModalButtonText>Yes</ModalButtonText>
-            </ModalButton>
-            <ModalButton onClick={toggleModal}>
-              <ModalButtonText>No</ModalButtonText>
-            </ModalButton>
-          </ModalButtonContainer>
-        </ModalWrapper>
-      </ModalContainer>
+      <div className={styles.logoutModalContainer}>
+        <div className={styles.logoutModalWrapper}>
+          <p className={styles.logoutModalText}>{modalText}</p>
+          <div className={styles.logoutModalButtonContainer}>
+            <button className={styles.logoutModalButton} onClick={handleUserLogout}>
+              <span className={styles.logoutModalButtonText}>Yes</span>
+            </button>
+            <button className={styles.logoutModalButton} onClick={toggleModal}>
+              <span className={styles.logoutModalButtonText}>No</span>
+            </button>
+          </div>
+        </div>
+      </div>
     )
   );
 };
