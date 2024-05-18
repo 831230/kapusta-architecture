56b114dbf9b978badf339ee983536402b2d7fbce
Piotr Poterek <peterpoterek0@gmail.com>
2024-05-19 00:30:27 +0200

    date input wip

--- src/components/ExpensesIncomeForm/ExpensesIncomeForm.jsx
+++ src/components/ExpensesIncomeForm/ExpensesIncomeForm.jsx
@@ -1,27 +1,69 @@
-import { useState } from "react";
-import { useDispatch } from "react-redux";
+import { useState, useRef } from 'react';
+import { useDispatch } from 'react-redux';
 
-const ExpensesIncomeForm = ({callback, actionType}) => {
-  const [date, setDate] = useState("");
-  const [description, setDescription] = useState("");
-  const [category, setCategory] = useState("");
-  const [amount, setAmount] = useState("");
+import styles from './ExpensesIncomeForm.module.css';
+import calendar from '../../assets/calendar.svg';
+
+const ExpensesIncomeForm = ({ callback, actionType }) => {
+  const [date, setDate] = useState('');
+  const [description, setDescription] = useState('');
+  const [category, setCategory] = useState('');
+  const [amount, setAmount] = useState('');
 
   const dispatch = useDispatch();
+  const dateInputRef = useRef(null);
+
+  const getTodayDate = () => {
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = String(today.getMonth() + 1).padStart(2, '0');
+    const day = String(today.getDate()).padStart(2, '0');
+    return `${year}-${month}-${day}`;
+  };
 
   const sendNewExpenseIncome = () => {
     const data = { date, description, category, amount };
-    dispatch(callback(data))
-  }
+
+    console.log(data);
+    dispatch(callback(data));
+  };
+
+  const handleCalendarClick = () => {
+    dateInputRef.current.showPicker();
+  };
 
   return (
-    <form action="">
-      <input type="date" onChange={(e) => setDate(e.target.value)} value={date}/>
-      <input type="text" placeholder="Product description" onChange={(e) => setDescription(e.target.value)} value={description}/>
-      <input type="text" placeholder="Product category" onChange={(e) => setCategory(e.target.value)} value={category}/>
-      <input type="number" placeholder="0,00" onChange={(e) => setAmount(e.target.value)} value={amount}/>
-      <button type="button" onClick={sendNewExpenseIncome}>Input</button>
-      <button>Clear</button>
+    <form className={styles.ExpensesIncomeForm} action="">
+      <div className={styles.ExpensesIncomeFormDateContainer} onClick={handleCalendarClick}>
+        <img className={styles.ExpensesIncomeFormDateIcon} src={calendar} alt="calendar icon" />
+        <input
+          ref={dateInputRef}
+          className={styles.ExpensesIncomeFormDateInput}
+          type="date"
+          onChange={(e) => setDate(e.target.value)}
+          value={date}
+        />
+      </div>
+      <input
+        type="text"
+        placeholder="Product description"
+        onChange={(e) => setDescription(e.target.value)}
+        value={description}
+      />
+      <input
+        type="text"
+        placeholder="Product category"
+        onChange={(e) => setCategory(e.target.value)}
+        value={category}
+      />
+      <input type="number" placeholder="0,00" onChange={(e) => setAmount(e.target.value)} value={amount} />
+
+      <div>
+        <button type="button" onClick={sendNewExpenseIncome}>
+          Input
+        </button>
+        <button>Clear</button>
+      </div>
     </form>
   );
 };
