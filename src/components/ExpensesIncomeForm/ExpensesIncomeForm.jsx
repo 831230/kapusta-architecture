9b984d54ae2fbcd4a7825b3d9bf217a1979bbe3e
Piotr Poterek <peterpoterek0@gmail.com>
2024-05-19 17:47:44 +0200

    select input

--- src/components/ExpensesIncomeForm/ExpensesIncomeForm.jsx
+++ src/components/ExpensesIncomeForm/ExpensesIncomeForm.jsx
@@ -1,10 +1,16 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import CreateDatePicker from '../DatePickerForm/DatePicker';
+import Select from 'react-select';
 
 import styles from './ExpensesIncomeForm.module.css';
 
-const ExpensesIncomeForm = ({ callback, actionType }) => {
+const categoryOptions = [
+  { value: 'products', label: 'Products' },
+  { value: 'alcohol', label: 'Alcohol' },
+];
+
+const ExpensesIncomeForm = ({ callback, actionType, categories }) => {
   const [date, setDate] = useState('');
   const [description, setDescription] = useState('');
   const [category, setCategory] = useState('');
@@ -16,6 +22,26 @@ const ExpensesIncomeForm = ({ callback, actionType }) => {
     const data = { date, description, category, amount };
     console.log(data);
     dispatch(callback(data));
+  };
+
+  const handleCategoryChange = (selectedOption) => {
+    setCategory(selectedOption ? selectedOption.value : '');
+  };
+  const customSelectStyles = {
+    control: (provided, state) => ({
+      ...provided,
+      zIndex: 5,
+      height: '50px',
+      borderColor: state.isFocused ? '#FF751D' : provided.borderColor,
+      '&:hover': {
+        borderColor: '#FF751D',
+      },
+      boxShadow: state.isFocused ? '0 0 0 1px #FF751D' : provided.boxShadow,
+    }),
+    menu: (provided) => ({
+      ...provided,
+      zIndex: 5,
+    }),
   };
 
   return (
@@ -30,12 +56,15 @@ const ExpensesIncomeForm = ({ callback, actionType }) => {
           onChange={(e) => setDescription(e.target.value)}
           value={description}
         />
-        <input
-          type="text"
-          placeholder="Product category"
-          onChange={(e) => setCategory(e.target.value)}
-          value={category}
+        <Select
+          className={styles.ExpensesIncomeFormInputSelectCategory}
+          options={categoryOptions}
+          onChange={handleCategoryChange}
+          value={categoryOptions.find((option) => option.value === category)}
+          placeholder="Product Category"
+          styles={customSelectStyles}
         />
+
         <input type="number" placeholder="0,00" onChange={(e) => setAmount(e.target.value)} value={amount} />
       </div>
       <button type="button" onClick={sendNewExpenseIncome}>
